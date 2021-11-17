e7f52ba16df6e645f88d31e7a9cc4f8c0de7334c
Vaishak <vaishak140498@gmail.com>
2021-11-17 18:28:35 +0800

    change time loop to while loop

--- bot.js
+++ bot.js
@@ -14,14 +14,11 @@ bot.command('/stopjoe', async (ctx) => {
 
 bot.command('/tradingjoe', async (ctx) => {
     plsStop = false;
-    var timer = setInterval(async () => {
-        if (plsStop) {
-            clearInterval(timer);
-        }
+
+    while (!plsStop) {
         console.log('Running');
-        let arr = [];
         try {
-            arr = await joe(['0.25', '0.3', '0.35', '0.4'], 'wsOHM', 'MIM');
+            let arr = await joe(['0.25', '0.3', '0.35', '0.4'], 'wsOHM', 'MIM');
             let len = arr.length;
             let i = 0;
             let message = '';
@@ -34,9 +31,19 @@ bot.command('/tradingjoe', async (ctx) => {
                 parse_mode: 'HTML',
             });
         } catch (error) {
-            await bot.telegram.sendMessage(ctx.chat.id, `Error in getting data: ${error}`, {});
+            await bot.telegram.sendMessage(
+                ctx.chat.id,
+                `Error in getting data: ${error}`,
+                {}
+            );
         }
-    }, 30000);
+    }
+
+    await bot.telegram.sendMessage(
+        ctx.chat.id,
+        `Joe has stopped.`,
+        {}
+    );
 });
 
 module.exports = bot;
