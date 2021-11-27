2680356c523962a1bc419e56e204f1120cd9053f
Vaishak <vaishak140498@gmail.com>
2021-11-27 12:03:34 +0800

    Add buy alert feature

--- controllers/traderjoe.js
+++ controllers/traderjoe.js
@@ -4,7 +4,8 @@ const getNormalTime = require('../util/datetime');
 
 let plsStop = false;
 let messageId = null;
-let alertLimit = 33500.0;
+let sellAlertLimit = 33500.0;
+let buyAlertLimit = 100.0;
 
 bot.command('/stopjoe', async (ctx) => {
     console.log('Stopping joe');
@@ -20,38 +21,42 @@ const sendJoeData = async (ctx) => {
         }
         let arr = await joe(['0.3', '0.35', '0.4'], 'wsOHM', 'MIM');
         let i = 0;
-        let isAlert = false;
+        let isSellAlert = false;
+        let isBuyAlert = false;
+        let alertPrice = [];
+
         let message = `Retrieval Time: ${getNormalTime()}\n\n`;
         while (i < arr.length) {
             let amountOfOhm = arr[i++];
             let buyPrice = arr[i++];
             let sellPrice = arr[i++];
-            if (!isAlert && sellPrice != null) {
-                isAlert = parseFloat(sellPrice) > alertLimit;
+            if (!isSellAlert && sellPrice != null) {
+                isSellAlert = parseFloat(sellPrice) >= sellAlertLimit;
+                if (isSellAlert) {
+                    alertPrice.push(buyPrice, sellPrice);
+                }
             }
+            if (!isBuyAlert && !isSellAlert && buyPrice != null) {
+                isBuyAlert = parseFloat(buyPrice) <= buyAlertLimit;
+                if (isBuyAlert) {
+                    alertPrice.push(buyPrice, sellPrice);
+                }
+            }
+
             message += `<b>${amountOfOhm} OHM:</b>\nBuy Price: ${buyPrice}\nSell Price: ${sellPrice}\n\n`;
             amountOfOhm = null;
             buyPrice = null;
             sellPrice = null;
         }
         arr = null;
-        if (isAlert) {
-            message = '<b>ALERT</b> @Vforvitagen\n' + message;
-        }
 
-        if (messageId == null) {
+        if (messageId == null && !plsStop) {
             await bot.telegram
                 .sendMessage(ctx.chat.id, message, {
                     parse_mode: 'HTML',
                 })
                 .then((messageDetails) => {
                     messageId = messageDetails.message_id;
-                })
-                .catch((err) => console.error(err));
-        } else if (isAlert) {
-            await bot.telegram
-                .sendMessage(ctx.chat.id, message, {
-                    parse_mode: 'HTML',
                 })
                 .catch((err) => console.error(err));
         } else {
@@ -61,6 +66,22 @@ const sendJoeData = async (ctx) => {
                 })
                 .catch((err) => console.error(err));
         }
+
+        if (isSellAlert || isBuyAlert) {
+            let alertMessage = `<b>${
+                isSellAlert ? 'SELL' : 'BUY'
+            } ALERT</b> @Vforvitagen\nBuy Price: ${
+                alertPrice[0]
+            }\nSell Price: ${alertPrice[1]}`;
+
+            await bot.telegram
+                .sendMessage(ctx.chat.id, alertMessage, {
+                    parse_mode: 'HTML',
+                })
+                .catch((err) => console.error(err));
+        }
+
+        alertPrice = null;
         message = null;
     } catch (error) {
         console.error(error);
@@ -75,20 +96,39 @@ bot.command('/tradingjoe', async (ctx) => {
     sendJoeData(ctx);
 });
 
-bot.command('/setjoealert', async (ctx) => {
-    console.log('Setting alert');
+bot.command('/sellalert', async (ctx) => {
+    console.log('Setting sell alert');
     const args = ctx.update.message.text.split(' ');
     if (args.length != 2) {
         bot.telegram.sendMessage(
             ctx.chat.id,
-            'Please input the correct format!\nE.g: /setjoealert 30000',
+            'Please input the correct format!\nE.g: /sellalert 30000',
             {}
         );
     } else {
-        alertLimit = parseFloat(args[1]);
+        sellAlertLimit = parseFloat(args[1]);
         bot.telegram.sendMessage(
             ctx.chat.id,
-            `Set the alert floor to ${alertLimit}`,
+            `Set the sell alert floor to ${sellAlertLimit}`,
+            {}
+        );
+    }
+});
+
+bot.command('/buyalert', async (ctx) => {
+    console.log('Setting buy alert');
+    const args = ctx.update.message.text.split(' ');
+    if (args.length != 2) {
+        bot.telegram.sendMessage(
+            ctx.chat.id,
+            'Please input the correct format!\nE.g: /buyalert 3000',
+            {}
+        );
+    } else {
+        buyAlertLimit = parseFloat(args[1]);
+        bot.telegram.sendMessage(
+            ctx.chat.id,
+            `Set the buy alert ceiling to ${buyAlertLimit}`,
             {}
         );
     }
